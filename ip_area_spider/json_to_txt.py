0c7b12d50f7483bd18370371b382ce487d781ce4
Yuxuecheng <yuxuecheng@baicdata.com>
2017-09-06 15:58:04 +0800

    Update the json_to_txt.py script, Now it can generate sql file for update the mysql database.

--- ip_area_spider/json_to_txt.py
+++ ip_area_spider/json_to_txt.py
@@ -12,7 +12,9 @@
 
 import json
 import os
+import sys
 import socket
+import struct
 
 
 class IpRange(object):
@@ -26,12 +28,12 @@ class IpRange(object):
             self.end_ip[3] = 255
             self.area_id = area_id
             self.area_name = area_name
-            self.start_ip_long = socket.inet_aton('.'.join(map(str, self.start_ip)))
-            self.end_ip_long = socket.inet_aton('.'.join(map(str, self.end_ip)))
+            self.start_ip_long = convert_ip_to_long('.'.join(map(str, self.start_ip)))
+            self.end_ip_long = convert_ip_to_long('.'.join(map(str, self.end_ip)))
             self.orig_start_ip = orig_start_ip
             self.orig_end_ip = orig_end_ip
-            self.insert_sql_pattern = 'INSERT INTO rmc_area_ip_fixed (id, area_name, start_ip, start_ip_long, end_ip, end_ip_long, type) VALUES ({0}, "{1}", "{2}", INET_ATON("{2}"), "{3}", INET_ATON("{3}"), 0);'
-            self.update_sql_pattern = 'UPDATE rmc_area_ip_fixed set id={0} where kid={1};'
+            self.insert_sql_pattern = 'INSERT INTO rmc_area_ip_fixed (id, area_name, start_ip, start_ip_long, end_ip, end_ip_long, type) VALUES ("{0}", "{1}", "{2}", INET_ATON("{2}"), "{3}", INET_ATON("{3}"), 0);'
+            self.update_sql_pattern = 'UPDATE rmc_area_ip_fixed set id="{0}", area_name="{1}" where kid={2};'
             assert self.start_ip_long < self.end_ip_long
 
     def __eq__( self, other ):
@@ -44,7 +46,7 @@ class IpRange(object):
         else:
             return False
 
-    def __cmp__( self, other ):
+    def __cmp__(self, other):
         assert isinstance(other, IpRange)
         if self.start_ip_long == other.start_ip_long and self.end_ip_long == other.end_ip_long:
             return 0
@@ -52,11 +54,13 @@ class IpRange(object):
             return -1
         elif self.start_ip_long > other.end_ip_long:
             return 1
+        else:
+            print self, other
 
     def __repr__( self ):
-        display = "区域ID: {0}, 区域名称: {1}, 起始IP: {2}, 结束IP: {3}".format(self.area_id, self.area_name,
-                                                                      '.'.join(map(str, self.start_ip)),
-                                                                      '.'.join(map(str, self.end_ip)))
+        display = "区域ID: {0}, 区域名称: {1}, 起始IP: {2}, 起始IP数字：{3}, 结束IP: {4}, 结束IP数字：{5}".format(self.area_id, self.area_name,
+                                                                      '.'.join(map(str, self.start_ip)), self.start_ip_long,
+                                                                      '.'.join(map(str, self.end_ip)), self.end_ip_long)
         display += os.linesep
         return display
 
@@ -152,7 +156,7 @@ class IpRange(object):
         start_ip_str = '.'.join(map(str, self.start_ip))
         end_ip_str = '.'.join(map(str, self.end_ip))
         if self.orig_start_ip == start_ip_str and self.orig_end_ip == end_ip_str:
-            return self.update_sql_pattern.format(self.area_id, self.kid)
+            return self.update_sql_pattern.format(self.area_id, self.area_name, self.kid)
         else:
             return self.insert_sql_pattern.format(int(self.area_id), self.area_name, start_ip_str, end_ip_str)
 
@@ -168,10 +172,20 @@ def ip_decompose( ip ):
         return map(int, ip.split('.'))
 
 
+# 从IP地址字符串转换为整数值
+def convert_ip_to_long(ip_string):
+    return struct.unpack("!L", socket.inet_aton(ip_string))[0]
+
+
+# 从网络字节序的数字转换为IP地址（点号分隔）
+def convert_long_to_ip(ip):
+    return socket.inet_ntoa(struct.pack("!L", ip))
+
+
 def analysis_ip_range():
     global id_area_map
     ip_range_list = list()
-    with open("ip_area_id_test.json", mode="r") as fd:
+    with open("ip_area_id.json", mode="r") as fd:
         for line in fd:
             line = line.strip()
             record = json.loads(line)
@@ -188,7 +202,7 @@ def analysis_ip_range():
             ip_range_list.append(ip_range)
 
     sorted_ip_list = sorted(ip_range_list)
-    print(sorted_ip_list)
+    # print(sorted_ip_list)
 
     merge_ip_range_list = list()
     index = 0
@@ -203,22 +217,98 @@ def analysis_ip_range():
         # print("After merge: ", ip_range)
         merge_ip_range_list.append(ip_range)
 
-    print("final result: ")
-    print(merge_ip_range_list)
+    # print("final result: ")
+    # print(merge_ip_range_list)
     return merge_ip_range_list
 
 
-def main():
-    with open("ip_area_id.json", mode="r") as fd:
+def load_old_ip_range():
+    orig_ip_range_list = list()
+    with open("rmc_area_ip_orig.txt", mode="r") as fd:
         for line in fd:
             line = line.strip()
-            record = json.loads(line)
-            if record["id"] == record["prev_id"]:
-                continue
+            kid, id, area_name, start_ip, end_ip = line.split("\t")
+            ip_range = IpRange(kid=kid, area_id=id, area_name=area_name, orig_start_ip=start_ip, orig_end_ip=end_ip, start_ip=start_ip, end_ip=end_ip)
+            # print(ip_range)
+            orig_ip_range_list.append(ip_range)
 
-            # print u"\t".join([record["ip"], record["id"], record["area_name"].encode('utf-8'), record["start_ip"], record["end_ip"]])
-            # print record['area_name']
-            print(record["ip"], record["id"], record["area_name"].encode('utf-8'), record["start_ip"], record["end_ip"])
+    return orig_ip_range_list
+
+
+def merge_with_old(new_ip_list, old_ip_list):
+    with open("update_rmc_new2.sql", mode="w") as out:
+        for old_ip_range in old_ip_list:
+            old_start_ip_long = old_ip_range.start_ip_long
+            old_end_ip_long = old_ip_range.end_ip_long
+            expect_start_ip_long = old_start_ip_long
+            pre_end_ip_long = 0
+            match_start = False
+            match_end = False
+            """
+            mismatch 表示匹配过程中
+            """
+            mismatch = False
+            """
+            has_update 表示是否已经使用过UPDATE语句占用原来的kid，一个old_ip_range只能有一个UPDATE，其他的都是INSERT
+            """
+            has_update = False
+            for new_ip_range in new_ip_list:
+                new_start_ip_long = new_ip_range.start_ip_long
+                new_end_ip_long = new_ip_range.end_ip_long
+                if new_start_ip_long < old_start_ip_long:
+                    continue
+                if new_end_ip_long > old_end_ip_long:
+                    break
+
+                if new_start_ip_long == old_start_ip_long and new_end_ip_long == old_end_ip_long:
+                    if has_update is True:
+                        print("Has error, has update is already True. %s, %s", old_ip_range, new_ip_range)
+                        out.close()
+                        return 0
+                    sql_statement = 'UPDATE rmc_area_ip_fixed set id="{0}", area_name="{1}" where kid={2};'.format(new_ip_range.area_id, new_ip_range.area_name, old_ip_range.kid)
+                    out.write(sql_statement)
+                    out.write(os.linesep)
+                    break
+                if new_start_ip_long == old_start_ip_long:
+                    match_start = True
+                if new_end_ip_long == old_end_ip_long:
+                    match_end = True
+
+                if has_update is False:
+                    if new_start_ip_long > old_start_ip_long:
+                        expect_start_ip_long = new_end_ip_long + 1
+                        sql_statement = 'UPDATE rmc_area_ip_fixed set end_ip=INET_NTOA({0}), end_ip_long={0} WHERE kid={1};'.format(new_start_ip_long - 1, old_ip_range.kid)
+                        out.write(sql_statement)
+                        out.write(os.linesep)
+                        sql_statement = 'INSERT INTO rmc_area_ip_fixed (id, area_name, start_ip, start_ip_long, end_ip, end_ip_long, type) VALUES ("{0}", "{1}", INET_NTOA({2}), {2}, INET_NTOA({3}), {3}, 0);'.format(new_ip_range.area_id, new_ip_range.area_name, new_ip_range.start_ip_long, new_ip_range.end_ip_long)
+                        out.write(sql_statement)
+                        out.write(os.linesep)
+                        has_update = True
+                    elif new_start_ip_long == old_start_ip_long:
+                        expect_start_ip_long = new_end_ip_long + 1
+                        sql_statement = 'UPDATE rmc_area_ip_fixed set end_ip=INET_NTOA({0}), end_ip_long={0} WHERE kid={1};'.format(new_end_ip_long, old_ip_range.kid)
+                        out.write(sql_statement)
+                        out.write(os.linesep)
+                        has_update = True
+                else:
+                    if new_start_ip_long == expect_start_ip_long:
+                        sql_statement = 'INSERT INTO rmc_area_ip_fixed (id, area_name, start_ip, start_ip_long, end_ip, end_ip_long, type) VALUES ("{0}", "{1}", INET_NTOA({2}), {2}, INET_NTOA({3}), {3}, 0);'.format(new_ip_range.area_id, new_ip_range.area_name, new_ip_range.start_ip_long, new_ip_range.end_ip_long)
+                        out.write(sql_statement)
+                        out.write(os.linesep)
+                        expect_start_ip_long = new_end_ip_long + 1
+                    elif new_start_ip_long > expect_start_ip_long:
+                        sql_statement = 'INSERT INTO rmc_area_ip_fixed (id, area_name, start_ip, start_ip_long, end_ip, end_ip_long, type) VALUES ("{0}", "{1}", INET_NTOA({2}), {2}, INET_NTOA({3}), {3}, 0);'.format(old_ip_range.area_id, old_ip_range.area_name, expect_start_ip_long, new_start_ip_long - 1)
+                        out.write(sql_statement)
+                        out.write(os.linesep)
+                        sql_statement = 'INSERT INTO rmc_area_ip_fixed (id, area_name, start_ip, start_ip_long, end_ip, end_ip_long, type) VALUES ("{0}", "{1}", INET_NTOA({2}), {2}, INET_NTOA({3}), {3}, 0);'.format(new_ip_range.area_id, new_ip_range.area_name, new_ip_range.start_ip_long, new_ip_range.end_ip_long)
+                        out.write(sql_statement)
+                        out.write(os.linesep)
+                        expect_start_ip_long = new_end_ip_long + 1
+
+                if expect_start_ip_long > old_end_ip_long:
+                    break
+        out.flush()
+        out.close()
 
 
 def load_id_map():
@@ -244,11 +334,8 @@ id_area_map = load_id_map()
 if __name__ == "__main__":
     # print ip_check("192.18.12.1")
     # print ip_check("21.323.22.232")
+    orig_ip_range = load_old_ip_range()
+    sorted_orig_ip_list = sorted(orig_ip_range)
     ip_list = analysis_ip_range()
-    with open('update_rmc.sql', mode='w') as fd:
-        for ip_range in ip_list:
-            sql_statement = ip_range.generate_sql()
-            fd.write(sql_statement)
-            fd.write(os.linesep)
-
-            # main()
+    merge_with_old(ip_list, sorted_orig_ip_list)
+    sys.exit(0)
